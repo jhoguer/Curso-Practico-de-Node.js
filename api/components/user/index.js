db1f2c1e83e40905844090ef85f6c29a5d59d470
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-25 11:30:29 -0500

    Desplegando Microservicios en Zeit Now, serverless y seguridad

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,14 @@
-const ctrl = require('./controller')
 // const store = require('../../../store/mysql')
-const store = require('../../../store/remote-mysql')
+const config = require('../../../config');
+
+
+let store;
+if (config.remoteDB == true) {
+  store = require('../../../store/remote-mysql');
+} else {
+  store = require('../../../store/mysql')
+}
+
+const ctrl = require('./controller')
 
 module.exports = ctrl(store)
