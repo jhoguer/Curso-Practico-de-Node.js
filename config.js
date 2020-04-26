06e9e8af85fd3803ba96138d24950dc066bc4e7e
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-25 21:19:52 -0500

    Conectando el servicio a redis

--- config.js
+++ config.js
@@ -22,5 +22,10 @@ module.exports = {
   cacheService: {
     host: process.env.MYSQL_SRV_HOST || 'localhost',
     port: process.env.MYSQL_SRV_PORT || 3004,
+  },
+  redis: {
+    host: process.env.REDIS_HOST || 'redis-13556.c15.us-east-1-4.ec2.cloud.redislabs.com',
+    port: process.env.REDIS_PORT || 13556,
+    password: process.env.REDIS_PASS || 'T2WsAklcNGjMea3YUHkjHY7PXYGbUJfT'
   }
 }
